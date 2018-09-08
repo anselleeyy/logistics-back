94b964b8cb8291eb86175aa7cef0953c28dc4dfe
anselleeyy <30712612+anselleeyy@users.noreply.github.com>
2018-09-08 17:27:32 +0800

    fix bug

--- src/main/java/com/ansel/controller/ClearController.java
+++ src/main/java/com/ansel/controller/ClearController.java
@@ -1,7 +1,5 @@
 package com.ansel.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
